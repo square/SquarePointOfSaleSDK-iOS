496a75da2f9c52a153c795a10be94b635a2945e3
Gareth Jones <gareth@garethpaul.com>
2016-06-29 17:21:37 -0700

    adding sample app for swift

---
+++ SquareRegisterSDK-Test-App-Swift/SquareRegisterSDK-Test-App-Swift-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  SquareRegisterSDK-Test-App-Swift-Bridging-Header.h
+//  SquareRegisterSDK Tests
+//
+//  Created by Gareth on 6/29/16.
+//  Copyright © 2016 Square, Inc. All rights reserved.
+//
+#import "SquareRegisterSDK.h"
